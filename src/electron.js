ad0017db375ed2b7051c580d6ad3314e8cb9e1c5
titanht <hayelomtizazu19@gmail.com>
2019-04-26 17:47:18 +0300

    Electron react boilerplate

---
+++ src/electron.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1100, 
+    height: 700,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: false
+    }
+  });
+
+  mainWindow.loadURL(isDev ? 'http://localhost:3003' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
